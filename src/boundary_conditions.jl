0f1aecca687894934f2d5b9baf3d659857aab0e0
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-23 14:59:20 -0400

    Deletes accidentally duplicated code
    Former-commit-id: 55a385302f1a5e07ea2e3eaa45d8b52df0c4f791

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -186,39 +186,6 @@ getbc(cbc::CBC, ::Val{S}) where S = getfield(cbc, S)
 getbc(cbc::CBC, ::Val{:bottom}) = getfield(cbc, :left)
 
 #####
-##### Boundary conditions along particular coordinates
-#####
-
-"""
-    CoordinateBoundaryConditions(left, right)
-
-A set of two `BoundaryCondition`s to be applied along a coordinate x, y, or z.
-
-The `left` boundary condition is applied on the negative or lower side of the coordinate
-while the `right` boundary condition is applied on the positive or higher side.
-"""
-mutable struct CoordinateBoundaryConditions{L, R}
-     left :: L
-    right :: R
-end
-
-const CBC = CoordinateBoundaryConditions
-PeriodicBCs() = CBC(PeriodicBC(), PeriodicBC())
-
-# Here we overload setproperty! and getproperty to permit users to call
-# the 'left' and 'right' bcs in the z-direction 'bottom' and 'top'.
-# Note that 'right' technically corresponds to face point N+1.
-Base.setproperty!(cbc::CBC, side::Symbol, bc) = setbc!(cbc, Val(side), bc)
-setbc!(cbc::CBC, ::Val{S}, bc) where S = setfield!(cbc, S, bc)
-setbc!(cbc::CBC, ::Val{:bottom}, bc) = setfield!(cbc, :left, bc)
-setbc!(cbc::CBC, ::Val{:top}, bc) = setfield!(cbc, :right, bc)
-
-Base.getproperty(cbc::CBC, side::Symbol) = getbc(cbc, Val(side))
-getbc(cbc::CBC, ::Val{S}) where S = getfield(cbc, S)
-getbc(cbc::CBC, ::Val{:bottom}) = getfield(cbc, :left)
-getbc(cbc::CBC, ::Val{:top}) = getfield(cbc, :right)
-
-#####
 ##### Boundary conditions for Fields
 #####
 
